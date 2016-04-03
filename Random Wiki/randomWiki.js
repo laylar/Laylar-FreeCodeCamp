140abfdee2e87cb89b3e11cb635b2c322b33b971
Layla Rowen <layla.rowen@gmail.com>
2016-04-02 17:29:26 -0700

    adding randomWiki files

---
+++ Random Wiki/randomWiki.js
@@ -0,0 +1,19 @@
+var wikiBaseLink = "https://en.wikipedia.org/wiki/";
+var wikiQuery = "";
+
+function relocate() {
+	//aquire submitted value and modify it
+  	wikiQuery = document.getElementById('wikiQueryValue').value.split(' ').join('_');
+  	url = wikiBaseLink + wikiQuery;
+  	alert("You\'re about to check out: " + url);
+	//navigate browser to related wiki link
+	location.href = url;
+	return false;
+}
+
+$(document).ready(function(){
+  $("#queryForm").submit(function(){
+  	
+  });
+  
+});
