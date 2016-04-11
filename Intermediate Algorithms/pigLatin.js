fa21c629b0fd952eff2d788d1ccbca079fb22712
Layla Rowen <layla.rowen@gmail.com>
2016-04-10 21:27:07 -0700

    adding details for searchAndReplace and pigLatin

--- Intermediate Algorithms/pigLatin.js
+++ Intermediate Algorithms/pigLatin.js
@@ -1,1 +1,22 @@
-alert("Hello!");
+function translatePigLatin(str) {
+  return str;
+}
+
+translatePigLatin("consonant");
+/*
+Translate the provided string to pig latin.
+
+Pig Latin takes the first consonant (or consonant cluster) of an English word, moves it to the end of the word and suffixes an "ay".
+
+If a word begins with a vowel you just add "way" to the end.
+
+
+//Suggested functions are:
+
+//Array.indexOf()
+//Array.push()
+//Array.join()
+//String.substr()
+//String.split()
+
+*/
